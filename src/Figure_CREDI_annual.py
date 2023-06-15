1a2e05668abc893223b3bd8fc26d41f3491e33f3
3986209 <l.p.stoop@uu.nl>
2023-06-15 15:05:31 +0200

    Increases

--- src/Figure_CREDI_annual.py
+++ src/Figure_CREDI_annual.py
@@ -9,6 +9,7 @@ Created on 2023-03-30
 
 
 # load the dependencies
+import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import xarray as xr
@@ -71,15 +72,30 @@ ds_WONanom = xr.open_dataset(FOLDER_project+'data/processed/ERA5_WON_clim-anom_P
 # =============================================================================
 
 
-# Make a CREBI dataset
-ds_SEBi = xr.Dataset()
-ds_WEBi = xr.Dataset()
+# # Make a CREBI dataset
+# ds_SEBi = xr.Dataset()
+# ds_WEBi = xr.Dataset()
 
 
-# Calculate the energy balance over the full period, but adjust to value on 1991-01-01
-ds_SEBi = ds_SPVanom.sel(time=slice('1991-01-01', '2020-12-31')).cumsum()
-ds_WEBi = ds_WONanom.sel(time=slice('1991-01-01', '2020-12-31')).cumsum()
+# # Calculate the energy balance over the full period, but adjust to value on 1991-01-01
+# ds_SEBi = ds_SPVanom.sel(time=slice('1991-01-01', '2020-12-31')).cumsum()
+# ds_WEBi = ds_WONanom.sel(time=slice('1991-01-01', '2020-12-31')).cumsum()
 
+
+
+# Initialize the dataframe
+df_Widx = pd.DataFrame()
+df_Sidx = pd.DataFrame()
+
+
+# we want to see all years
+for year in np.arange(start=1991,stop=2021):
+    
+    # Show the data for all the years
+    if year == 1991:
+        df_Widx[str(year)] = ds_WONanom.sel(time=slice(str(year)+'-04-01', str(year+1)+'-03-31')).cumsum().to_dataframe()
+    else:
+        df_Widx[str(year)] = ds_WONanom.sel(time=slice(str(year)+'-04-01', str(year+1)+'-03-31')).cumsum().values
 #%%
 # =============================================================================
 # Figure for interannual behaviour
@@ -101,8 +117,8 @@ fig.autofmt_xdate()
 ### First plot the energy balance index and additional lines 
 
 # we want to see the energy balance for all years
-axes['a)'].plot(ds_SPVanom.time.sel(time=slice('1991-01-01', '2020-12-31')), ds_WEBi.anom, color=colour_wind_credi)
-axes['b)'].plot(ds_SPVanom.time.sel(time=slice('1991-01-01', '2020-12-31')), ds_SEBi.anom,  color=colour_solar_credi)
+# axes['a)'].plot(ds_SPVanom.time.sel(time=slice('1991-01-01', '2020-12-31')), ds_WEBi.anom, color=colour_wind_credi)
+# axes['b)'].plot(ds_SPVanom.time.sel(time=slice('1991-01-01', '2020-12-31')), ds_SEBi.anom,  color=colour_solar_credi)
 
 
 # Add a line through zero
@@ -155,18 +171,7 @@ axes['b)'].plot(ds_SPVanom.time.sel(time=slice('1991-01-01', '2020-12-31')), ds_
 # OLD STUFF
 # =============================================================================
 
-# Initialize the dataframe
-df_WEBi = pd.DataFrame()
 
-
-# we want to see all years
-for year in np.arange(start=1991,stop=2021):
-    
-    # Show the data for all the years
-    if year == 1991:
-        df_WEBi[str(year)] = ds_WONanom.NL01.sel(time=slice(str(year)+'-05-01', str(year+1)+'-04-30')).cumsum().to_numpy()
-    else:
-        df_WEBi[str(year)] = ds_WONanom.NL01.sel(time=slice(str(year)+'-05-01', str(year+1)+'-04-30')).cumsum().values
 
 #%%
 # =============================================================================
@@ -175,7 +180,7 @@ for year in np.arange(start=1991,stop=2021):
 
 
 # we start a new figure
-fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(13,5), sharey=True)
+fig, axes = plt.subplot_mosaic([['a)'], ['b)']], figsize=(15,8))
 
 
 # fix date-format
@@ -184,42 +189,42 @@ fig.autofmt_xdate()
 ### First plot the initial yearly energy balance index and additional lines 
 
 # show years
-year_dates = pd.date_range('1990-05-01', periods=8760, freq='1h')
+year_dates = pd.date_range('1990-04-01', periods=8760, freq='1h')
 
 # we want to see all years
 for year in np.arange(start=1991,stop=2021):
     
     # Show the data for all the years
-    axes[0].plot(year_dates, df_WEBi[str(year)], color='dodgerblue', alpha=0.4, linewidth=1)
+    axes['a)'].plot(year_dates, df_WEBi[str(year)], color='dodgerblue', alpha=0.4, linewidth=1)
     
     
-axes[0].plot(year_dates,df_WEBi['1996'], color='red', label='1996', alpha=0.9, linewidth=1)
-axes[0].plot(year_dates,df_WEBi['2016'], color='purple', label='2016', alpha=0.9, linewidth=1)
+axes['a)'].plot(year_dates,df_WEBi['1996'], color='red', label='1996', alpha=0.9, linewidth=1)
+axes['a)'].plot(year_dates,df_WEBi['2016'], color='purple', label='2016', alpha=0.9, linewidth=1)
     
 
-axes[1].fill_between(
+axes['b)'].fill_between(
     year_dates, 
     df_WEBi.quantile(0,  axis=1),
     df_WEBi.quantile(1,  axis=1),
     color='dodgerblue', alpha=0.1, label='min-max'
     )
 
-axes[1].fill_between(
+axes['b)'].fill_between(
     year_dates, 
     df_WEBi.quantile(0.1,  axis=1),
     df_WEBi.quantile(0.9,  axis=1),
     color='dodgerblue', alpha=0.2, label='10-90%'
     )
 
-axes[1].fill_between(
+axes['b)'].fill_between(
     year_dates, 
     df_WEBi.quantile(0.25,  axis=1),
     df_WEBi.quantile(0.75,  axis=1),
     color='dodgerblue', alpha=0.4, label='25-75%'
     )
 
-axes[1].plot(year_dates,df_WEBi.quantile(0.5,  axis=1), color='dodgerblue', label='50%')
+axes['b)'].plot(year_dates,df_WEBi.quantile(0.5,  axis=1), color='dodgerblue', label='50%')
 
-axes[1].plot(year_dates,df_WEBi['1996'], color='red', label='1996', alpha=0.9, linewidth=1)
-axes[1].plot(year_dates,df_WEBi['2016'], color='purple', label='2016', alpha=0.9, linewidth=1)
-# axes[1].plot(year_dates,df_WEBi['2010'], color='red', label='2010', alpha=0.9, linewidth=1)
+axes['b)'].plot(year_dates,df_WEBi['1996'], color='red', label='1996', alpha=0.9, linewidth=1)
+axes['b)'].plot(year_dates,df_WEBi['2016'], color='purple', label='2016', alpha=0.9, linewidth=1)
+# axes['b)'].plot(year_dates,df_WEBi['2010'], color='red', label='2010', alpha=0.9, linewidth=1)
