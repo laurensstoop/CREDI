0f6bdb8f0307c05659f62f2167361f52b295a1a0
3986209 <l.p.stoop@uu.nl>
2023-06-21 03:11:50 +0200

    added subseason, not finished

--- src/Figure_CREDI_subseasonal.py
+++ src/Figure_CREDI_subseasonal.py
@@ -2,9 +2,9 @@
 """
 Spyder Editor
 
-Created on 2023-03-30
+Created on 2023-06-19
 
-Updated on 2023-06-19
+Updated on 2023-06-20
 
 @author: Laurens P. Stoop
 """
@@ -18,6 +18,7 @@ import xarray as xr
 import matplotlib.dates as mdates
 import matplotlib.transforms as mtransforms
 from datetime import timedelta
+import datetime
 
 
 
@@ -73,21 +74,31 @@ ds_WONanom = xr.open_dataset(FOLDER_project+'data/processed/ERA5_WON_clim-anom_P
 # =============================================================================
 
 # Initialize the dataframe
-df_WEBi = pd.DataFrame()
+df_WsS = pd.DataFrame()
+ds_WsSi = xr.Dataset()
 
 # we want to see all years
-for year in np.arange(start=1991,stop=2021):
+for year in np.arange(start=1991,stop=1992):
     
-    # Show the data for all the years
-    if year == 1991:
-        df_WEBi[str(year)] = ds_WONanom.anom.sel(time=slice(str(year)+'-09-01', str(year+1)+'-03-31')).cumsum().to_numpy()
-    else:
-        df_WEBi[str(year)] = ds_WONanom.anom.sel(time=slice(str(year)+'-09-01', str(year+1)+'-03-31')).cumsum().values
+    for seasonday in np.arange(1): #212
+        # Show the data for all the years
+        if year == 1991:
+            ds_WsSi[str(year)] = ds_WONanom.anom.sel(
+                time=slice(
+                    datetime.datetime(year,9,1,0,0) + timedelta(days=int(seasonday)), 
+                    datetime.datetime(year,9,15,0,0) + timedelta(days=int(seasonday))
+                    )).cumsum()
+        # else:
+        #     df_WEBi[str(year)] = ds_WONanom.anom.sel(time=slice(str(year)+'-09-01'+ timedelta(days=int(seasonday)), str(year+1)+'-03-31')+ timedelta(days=int(seasonday))).cumsum().values
 
 
 
+xr.DataArray(data, coords=[times, locs], dims=["time", "space"])
+pd.date_range("2000-01-01", periods=4)
+["IA", "IL", "IN"]
 
 
+#%%
 
 ds_subsW = xr.Dataset()
 
@@ -95,17 +106,6 @@ ds_subsW = xr.Dataset()
 year_dates = pd.date_range('1999-09-01',periods=5088, freq='1h')
 
 # we want to see all years
-for seasonday in np.arange(212):
-    
-    # Show the data for all the years
-    if seasonday == 1:
-        ds_subsW[str(year)] = ds_WONanom.anom.sel(time=slice(str(year)+'-09-01' + timedelta(days=int(seasonday)), str(year+1)+'-03-31')+ timedelta(days=int(seasonday))).cumsum().to_numpy()
-    else:
-        ds_subsW[str(year)] = ds_WONanom.anom.sel(time=slice(str(year)+'-09-01'+ timedelta(days=int(seasonday)), str(year+1)+'-03-31')+ timedelta(days=int(seasonday))).cumsum().values
-
-
-
-
 
 
 
@@ -163,8 +163,8 @@ axes['b)'].plot(year_dates,df_WEBi['1996'], color='red', alpha=0.9, linewidth=1)
 
    
 # # Add a line through zero
-axes['a)'].axhline(y=0.0, color='gray', linestyle='--')
-axes['b)'].axhline(y=0.0, color='gray', linestyle='--')
+# axes['a)'].axhline(y=0.0, color='gray', linestyle='--')
+# axes['b)'].axhline(y=0.0, color='gray', linestyle='--')
 
 # # add window markers
 # axes['a)'].vlines(x=['1991-01-01', '2020-12-31'], ymin=0, ymax=900, color='gray', alpha=0.6, linestyle='-.',  label='Climatology period')
@@ -178,42 +178,42 @@ axes['b)'].axhline(y=0.0, color='gray', linestyle='--')
 
 
 
-# formate the date-axis 
-xfmt_years = mdates.DateFormatter('%b')
-for a in ['a)', 'b)']:
-    axes[a].xaxis.set_major_locator(mdates.MonthLocator(interval=1))
-    axes[a].xaxis.set_minor_locator(mdates.MonthLocator())
-    axes[a].xaxis.set_major_formatter(xfmt_years)
+# # formate the date-axis 
+# xfmt_years = mdates.DateFormatter('%b')
+# for a in ['a)', 'b)']:
+#     axes[a].xaxis.set_major_locator(mdates.MonthLocator(interval=1))
+#     axes[a].xaxis.set_minor_locator(mdates.MonthLocator())
+#     axes[a].xaxis.set_major_formatter(xfmt_years)
 
 
 
 
-# # set the legend and labels
-# axes['a)'].legend(loc='lower right', fontsize='medium')
-axes['a)'].legend(loc='lower left', fontsize='medium')
-axes['b)'].legend(loc='lower left', fontsize='medium')
+# # # set the legend and labels
+# # axes['a)'].legend(loc='lower right', fontsize='medium')
+# axes['a)'].legend(loc='lower left', fontsize='medium')
+# axes['b)'].legend(loc='lower left', fontsize='medium')
 
 
 
-axes['a)'].set_ylim(-410,410)
-axes['b)'].set_ylim(-410,410)
+# axes['a)'].set_ylim(-410,410)
+# axes['b)'].set_ylim(-410,410)
 
-# # Fix labels
-axes['a)'].set_ylabel('Wind CREDI [FLH]')
-# axes['b)'].set_ylabel('Solar Energy Balance index')
-
-
-
-# # make it look better
-plt.tight_layout()
+# # # Fix labels
+# axes['a)'].set_ylabel('Wind CREDI [FLH]')
+# # axes['b)'].set_ylabel('Solar Energy Balance index')
 
 
-# print subplot names
-for label, ax in axes.items():
-    # label physical distance in and down:
-    trans = mtransforms.ScaledTranslation(10/72, -8/72, fig.dpi_scale_trans)
-    ax.text(0.0, 1.0, label, transform=ax.transAxes + trans,
-            fontsize='xx-large', verticalalignment='top')
+
+# # # make it look better
+# plt.tight_layout()
+
+
+# # print subplot names
+# for label, ax in axes.items():
+#     # label physical distance in and down:
+#     trans = mtransforms.ScaledTranslation(10/72, -8/72, fig.dpi_scale_trans)
+#     ax.text(0.0, 1.0, label, transform=ax.transAxes + trans,
+#             fontsize='xx-large', verticalalignment='top')
 
 # plt.savefig(FOLDER_project+'results/publication/WindCREDI_seasonal-winter.png')
 # plt.savefig(FOLDER_project+'results/publication/WindCREDI_seasonal-winter.pdf')
